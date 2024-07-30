f0b3bc70b2c6aec4ecc1dbff3c9409478e50565d
David Guzzi <dguzzi_10@hotmail.com>
2024-07-30 18:46:42 -0300

    Update AEM_examen_final.py

--- AEM_examen_final.py
+++ AEM_examen_final.py
@@ -40,3 +40,50 @@ path = r'C:\Users\HP\OneDrive\Escritorio\David Guzzi\Github\MECMT04\TP AEM - FD 
 df = pd.read_excel(path)
 
 df.iloc[:, 4:5].cov()
+#%%
+import pandas as pd
+import pyreadstat as st
+import numpy as np
+
+path = r"C:\Users\HP\OneDrive\Escritorio\David Guzzi\DiTella\MEC\Materias\2024 2T\[MT04] Análisis Estadístico Multivariado\Guías prácticas\Problem Set 1-20240714\eurosec.dta"
+
+df, meta = st.read_dta(path)
+
+# std = np.std(df.select_dtypes(include=[np.number]), axis=0, ddof=1)
+# mean = np.mean(df.select_dtypes(include=[np.number]), axis=0)
+
+# cv = std / mean
+
+# metrics = pd.concat([std, mean, cv], axis=1)
+# metrics.columns = ['desvío_estándar', 'promedio', 'coeficiente_de_variación']
+
+# cov = df.iloc[:,1:].cov()
+# pd.DataFrame(cov, index=df.select_dtypes(include=[np.number]).columns, 
+#                                    columns=df.select_dtypes(include=[np.number]).columns)
+
+# df.iloc[:,1:].cov()
+
+# cov = df.iloc[:,1:].cov()
+# np.linalg.det(cov)
+
+# corr = df.iloc[:,1:].corr()
+# eigenvalues, eigenvectors = np.linalg.eig(corr)
+# eigenvalues
+#%%
+import pandas as pd
+import pyreadstat as st
+import numpy as np
+
+path = r"C:\Users\HP\OneDrive\Escritorio\David Guzzi\DiTella\MEC\Materias\2024 2T\[MT04] Análisis Estadístico Multivariado\Guías prácticas\Problem Set 1-20240714\records.dta"
+
+df, meta = st.read_dta(path)
+desc = df.describe()
+
+dfn = df.iloc[:,1:]
+mean = dfn.mean()
+std = dfn.std()
+cv = std / mean
+
+desc_2 = pd.concat([mean, std, cv], axis=1)
+desc_2.columns = ['mean', 'standard_deviation', 'cv']
+desc_2
