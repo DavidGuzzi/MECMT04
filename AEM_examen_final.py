75190986bfa48d745b12c9933c2b0cf363bb9913
David Guzzi <dguzzi_10@hotmail.com>
2024-07-31 20:16:49 -0300

    changes

--- AEM_examen_final.py
+++ AEM_examen_final.py
@@ -79,6 +79,15 @@ path = r"C:\Users\HP\OneDrive\Escritorio\David Guzzi\DiTella\MEC\Materias\2024 2
 df, meta = st.read_dta(path)
 desc = df.describe()
 
+mean = df.iloc[:,1:].mean()
+std = df.iloc[:,1:].std()
+cv = std / mean
+
+a = pd.concat([mean, std, cv], axis=1)
+a.columns = ['mean', 'std', 'cv']
+
+df.iloc[:,1:].corr()
+#%%
 dfn = df.iloc[:,1:]
 mean = dfn.mean()
 std = dfn.std()
@@ -103,3 +112,56 @@ a = pca.explained_variance_
 proportion_of_variance = a / np.sum(a)
 
 pca.components_.T * np.sqrt(a)
+
+#%%
+import pandas as pd
+import numpy as np
+
+path = r"C:\Users\HP\OneDrive\Escritorio\David Guzzi\Github\MECMT04\TP AEM - FD DG.xlsx"
+df = pd.read_excel(path)
+ndf = df.iloc[:,1:].set_index('Country Code')
+
+#Para todos los datos
+desc = ndf.describe()
+cv = pd.DataFrame(ndf.std() / ndf.mean()).T
+cv.index = ['cv']
+desc = pd.concat([desc, cv], axis=0)
+
+#Para todos los datos sin Argentina
+ndf_1 = ndf.drop('ARG')
+desc_1 = ndf_1.describe()
+cv_1 = pd.DataFrame(ndf_1.std() / ndf_1.mean()).T
+cv_1.index = ['cv']
+desc_1 = pd.concat([desc_1, cv_1], axis=0)
+
+#Para Argentina
+ndf_2 = ndf.loc[['ARG']]
+
+"""Ver si ARG no se encuentra muy fuera del promedio de la OCDE"""
+
+#Matriz de covarianzas
+mcov = ndf.cov()
+
+#Matriz de correlaciones
+mcor = ndf.corr()
+
+#Medidas de variazión conjunta y medidas de correlación conjunta
+varianza_total = np.trace(mcov)
+varianza_media = varianza_total / len(ndf.columns)
+varianza_generalizada = np.linalg.det(mcov)
+varianza_efectiva = varianza_generalizada**(1/len(ndf.columns))
+
+dependencia_conjunta = np.linalg.det(mcor)
+dependencia_efectiva = dependencia_conjunta**(1/(len(ndf.columns)-1))
+
+medidas_globales = pd.DataFrame([[varianza_total, varianza_media, varianza_generalizada, varianza_efectiva, dependencia_conjunta, dependencia_efectiva]],
+                                columns=['varianza_total', 'varianza_media', 'varianza_generalizada', 'varianza_efectiva', 'dependencia_conjunta', 'dependencia_efectiva'])
+
+output_path = r'C:\Users\HP\OneDrive\Escritorio\David Guzzi\Github\MECMT04\OUTPUT_TP.xlsx'
+with pd.ExcelWriter(output_path) as writer:
+    desc.to_excel(writer, sheet_name='describe', index=False)
+    desc_1.to_excel(writer, sheet_name='describe 1', index=False)
+    ndf_2.to_excel(writer, sheet_name='describe 2', index=False)
+    mcov.to_excel(writer, sheet_name='covarianzas', index=False)
+    mcor.to_excel(writer, sheet_name='correlación', index=False)
+    medidas_globales.to_excel(writer, sheet_name='medidas_globales', index=False)
