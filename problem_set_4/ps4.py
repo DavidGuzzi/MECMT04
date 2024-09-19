e078d9cc99695fd58e49fee4b2362678f116e672
David Guzzi <dguzzi_10@hotmail.com>
2024-09-19 19:35:43 -0300

    DG

--- problem_set_4/ps4.py
+++ problem_set_4/ps4.py
@@ -1,8 +1,10 @@
 #%%
 import pandas as pd
 import pyreadstat as st
+import pingouin as pg
 
-path = r"C:\Users\HP\OneDrive\Escritorio\David Guzzi\Github\MECMT04\problem_set_3\firmas.dta"
+path = r"C:\Users\HP\OneDrive\Escritorio\David Guzzi\Github\MECMT04\problem_set_2\ine.dta"
 
 df, meta = st.read_dta(path)
-md = df.loc[:,['ebitass', 'rotc']]
+df.columns = [f"v{i}" for i in range(1, len(df.columns)+1)]
+mardia_test = pg.multivariate_normality(df, alpha=0.05)
